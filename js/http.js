cf770642851eec21ee0993224e6b74e75de85870
athome <35514901+haveataste@users.noreply.github.com>
2021-03-29 22:21:49 +0800

    Update http.js

--- js/http.js
+++ js/http.js
@@ -1,22 +1,19 @@
-const http = require('http');
-const fs = require('fs');
-
 http.createServer(function(request, response){
     var body = request.socket.remoteAddress.toString() + '\n' + request.socket.remotePort + '\n';
-
     var url = request.url;
     if(url === '/'){
         response.writeHead(200, {'Content-Type': 'text/plain;charset=UTF-8'});
+	    response.write(body);
         fs.readFile(__filename, (error, data) => {
             if(error){
                 response.write('Ops, error!');
             } else{
                 response.write(data);
             }
-            response.end(body);
+            response.end('----- END -----');
         });
     }
-}).listen(8080);
+}).listen(3000);
 
 console.log('Server running at http://127.0.0.1:8080/');
 
