2d30d0e0c8ccd171c3b6ea8a93f308fb6c70c1f4
athome <35514901+haveataste@users.noreply.github.com>
2022-04-23 15:14:20 +0800

    Update str.js

--- js/str.js
+++ js/str.js
@@ -1,21 +1,22 @@
 var s = 'abcdefghijklmn';
-s.charAt(0)
-s.charCodeAt(0)
+s.length
+s.charAt(0) === 'a'
+s.charCodeAt(0) === 97
+String.fromCharCode(65) === 'A'
+
+// 查找字符串位置起始索引值，找不到返回-1
 s.indexOf('efg', 0)
 s.lastIndexOf('jkl', 0)
 
-s.match(/bc/)
-//[ 'bc', index: 1, input: 'abcdefghijklmn', groups: undefined ]
-s.search(/cd/)
-//2
-s.replace(/bc/, 'qq')
-//'aqqdefghijklmn'
+s.match(/bc/)             //[ 'bc', index: 1, input: 'abcdefghijklmn', groups: undefined ]
+s.search(/cd/)            //2
+s.replace(/bc/, 'qq')     //'aqqdefghijklmn'
 
-s.split('')
-//截取字符串
-//str.substring(from[, to]) -- str.slice(start, end)
-//str.substr(from, length)
-//把字符串显示为下标和上标
+s.split('').join('-')
+// 截取字符串
+// str.substring(from[, to]) -- str.slice(start, end)
+// str.substr(from, length)
+// 把字符串显示为下标和上标
 s.sub(), s.sup()
 
 s.toLowerCase()
