52f4b2c7c307e461e87a0c8a1335fdfb70e427df
athome <35514901+haveataste@users.noreply.github.com>
2022-04-26 21:11:32 +0800

    Update file.js

--- js/file.js
+++ js/file.js
@@ -5,19 +5,17 @@ fs.open("fs.js", "r+", function(err, fd){
     if(err){return console.error(err);}
     console.log("file open success!");
 
-    // fs.read(fd, buffer, offset, length, position, callback);
     var buf = Buffer.alloc(1024*2);
+    // fs.read(fd, buffer, offset, length, position, callback);
     fs.read(fd, buf, 0, buf.length, 0, function(err, bytes, buf){
         if(err){return console.error(err);}
         console.log(bytes + " bytes read success!");
-        if(bytes > 0){
-            console.log(buf.slice(0, bytes).toString());
-        }
+        if(bytes > 0){console.log(buf.slice(0, bytes).toString());}
     });
 
-    // fs.write(fd, buffer, offset, length, position, callback);
     var content = Buffer.from(new String("/* Hello world! */"));
-    fs.write(fd, content, 0, content.length, 1090, function(err, bytes,
+    // fs.write(fd, buffer, offset, length, position, callback);
+    fs.write(fd, content, 0, content.length, 1090, function(err, bytes, buf){
         if(err){return console.error(err);}
         console.log("file write success!");
     });
