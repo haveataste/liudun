f85d1029913accd6e8c9e1eb0fd9eb3243e107f2
athome <35514901+haveataste@users.noreply.github.com>
2022-04-26 20:35:35 +0800

    Update str.js

--- js/str.js
+++ js/str.js
@@ -7,7 +7,7 @@ String.fromCharCode(65) === 'A'
 // 查找字符串位置起始索引值，找不到返回-1
 s.indexOf('efg', 0)
 s.lastIndexOf('jkl', 0)
-
+// 正则匹配
 s.match(/bc/)             //[ 'bc', index: 1, input: 'abcdefghijklmn', groups: undefined ]
 s.search(/cd/)            //2
 s.replace(/bc/, 'qq')     //'aqqdefghijklmn'
